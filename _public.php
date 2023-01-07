cccd921a02d4a5f00383737d0802e3e222deeabd
Pierre <brol31@gmail.com>
2023-01-07 02:34:09 +0100

    v1.8

--- _public.php
+++ _public.php
@@ -1,11 +1,15 @@
 <?php
-# ***** BEGIN LICENSE BLOCK *****
-#
-#  	Demo#1
-#  	Theme by Azork (http://xtradotfreedotfr.free.fr/blog/) and Pierre Van Glabeke
-#   License : public domain
-#
-# ***** END LICENSE BLOCK *****
+/**
+ * @brief Demo#1, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Theme
+ *
+ * @author Azork (http://xtradotfreedotfr.free.fr/blog/), Pierre Van Glabeke
+ *
+ * @copyright public domain
+ */
+ 
 if (!defined('DC_RC_PATH')) { return; }
 
 l10n::set(dirname(__FILE__) . '/locales/' . dcCore::app()->lang. '/public');
